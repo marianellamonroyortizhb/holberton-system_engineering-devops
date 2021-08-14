ec67427d15a755bcece4606091b4ab5a7b1c0d8e
marianellamonroyortizhb <1880@holbertonschool.com>
2021-08-14 03:51:58 +0000

    Remove newline in file 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create the file holberton
-file {'holberton':
+file {'create':
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
