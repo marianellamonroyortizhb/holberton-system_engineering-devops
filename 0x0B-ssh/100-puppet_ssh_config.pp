b491a5fbe69aa601cd300e5184ece5a94c764544
marianellamonroyortizhb <1880@holbertonschool.com>
2021-08-19 03:56:27 +0000

    Add PasswordAuthentication in file add 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,5 +3,10 @@
 file {'~/.ssh/holberton':
   path    => '/etc/ssh/holberton',
   mode    => '7000',
-  content => '$str',
+  content => 'Host default
+  HostName 104.196.185.70
+  User ubuntu
+  Port 22
+  PasswordAuthentication no
+  IdentifyFile /home/vagrant/.ssh/holberton',
 }
